5483f80d47d34fb269f5e009880fa1293be67a27
EloSanz <sanzeliseo@yahoo.com.ar>
2026-06-30 13:02:35 -0300

    chore: add deploy script and disable tracker-ui in compose to save RAM

---
+++ actualizar_front.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd ~/qisur-challenge
+
+echo "Obteniendo últimos cambios..."
+git pull
+
+echo "Reconstruyendo backend..."
+docker compose up -d --build
+
+echo "Construyendo frontend estático..."
+cd tracker-ui
+npm install
+npm run build
+
+echo "Copiando estáticos al Nginx del VPS..."
+mkdir -p /var/www/qisur
+rm -rf /var/www/qisur/*
+cp -r dist/* /var/www/qisur/
+
+echo "¡Qisur Challenge actualizado con éxito!"
